89e373183995536bfb7d6dbd0088be1bd5433d67
Mohammed Haji Hussain <mohammedhajihussain@Mohammeds-MacBook-Pro.local>
2026-04-27 15:34:12 +0530

    adds Money class and StringUtils methods; fix replaceAll usage
    - Implement immutable Money class with add and minus methods
    - Add StringUtils utilities: containsChar, reverse, replaceAll
    - Fix incorrect replaceAll method call in Main
    - Add additional output checks in Main

--- Money.java
+++ Money.java
@@ -1,1 +1,18 @@
 // Money.java
+
+class Money {
+  final int value;
+
+  Money(int value) {
+    this.value = value;
+  }
+
+  public Money add(Money addend) {
+    return new Money(this.value + addend.value);
+  }
+
+  public Money minus(Money subtractor) {
+    return new Money(this.value - subtractor.value);
+  }
+
+}
--- StringUtils.java
+++ StringUtils.java
@@ -1,1 +1,46 @@
 // StringUtils
+
+class StringUtils{
+  String value;
+
+  StringUtils(String value) {
+    this.value = value;
+  }
+
+  static boolean containsChar(String value, char c) {
+    for (int i = 0; i < value.length(); i++) {
+      char ch = value.charAt(i);
+      if(ch == c){
+        return true;
+      }
+    }
+    return false;
+  }
+
+  static String reverse(String value) {
+    String reversedString = "";
+
+    for (int i = value.length()-1; i >= 0 ; i--) {
+      char ch = value.charAt(i);
+      reversedString += ch;
+    }
+
+    return reversedString;
+  }
+
+  static String replaceAll(String value, char target, char replacer) {
+    String resultantString = "";
+
+    for (int i = 0; i < value.length(); i++) {
+      char ch = value.charAt(i);
+      if (ch == target) {
+        resultantString += replacer;
+      } else {
+        resultantString += ch;
+      }
+    }
+    
+    return resultantString;
+  }
+
+}
